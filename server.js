21bb94100a5fb67e59237cac4a8e6ea3e834e686
Vinny D <vdurham@prideseals.com>
2022-04-11 16:03:31 -0400

    added queries

--- server.js
+++ server.js
@@ -21,6 +21,31 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
 
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 app.use((req, res) => {
     res.status(404).end();
 });

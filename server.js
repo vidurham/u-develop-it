72c200496db9cc51c628fabe5cd8ba034b4b3387
Vinny D <vdurham@prideseals.com>
2022-04-11 15:52:37 -0400

    added query

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'lonewolf69',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();

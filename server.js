52ea130f70a837516384910dd296ce5b326bb0b2
Vinny D <vdurham@prideseals.com>
2022-04-13 11:47:32 -0400

    added parties to candidates

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const db = mysql.createConnection(
 );
 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
@@ -34,7 +38,13 @@ app.get('/api/candidates', (req, res) => {
 });
 
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
+                ON candidates.party_id = parties.id 
+                WHERE candidates.id = ?`;
+
     const params = [req.params.id];
     db.query(sql, params, (err, row) => {
         if (err) {
